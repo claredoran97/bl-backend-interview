6344a0176238a9b8e4c223b1bbc0f628465e82d5
claredoran97 <teamexact@hotmail.com>
2022-02-17 10:42:59 +0000

    task one complete pre comments

--- task_one/CommonPasswords.py
+++ task_one/CommonPasswords.py
@@ -1,38 +1,119 @@
 import re
+import ast
 from collections import Counter
+from itertools import groupby
+
 class CommonPasswords():
 	def __init__(self, password):
 		self.password = password
+		self.filePath = "../common-passwords.txt"
+		self.fullList = []
+		print()
 		print(password)
-		errorBool = True
-		while errorBool == True:
-			errorBool = self.lengthCheck()
-			print(errorBool)
-			# errorBool = False
-			errorBool = self.checkContains()
-			errorBool = self.occurenceCounter()
-			errorBool = False
 		self.result = 0
+		print()
+		self.passwordChecks()
+
+
+	def passwordChecks(self):
+		print("lengthCheck")
+		self.lengthCheck()
+		print("occurenceCounter")
+		self.occurenceCheckCounter()
+		print("loadCommonPasswords")
+		self.loadCommonPasswords()
 
 	def lengthCheck(self):
 		current_length = len(self.password)
+		substituteString = "1Z8lx5P"
 		print(current_length)
 		if current_length < 7:
-			return False
+			difference = 7 - len(self.password)
+			self.result += difference
+			substituteString = substituteString[-difference:]
+			self.password = self.password + substituteString
 		elif current_length > 12:
-			return False
-		else:
-			return True
+			difference = len(self.password) - 12
+			self.result += difference
+			self.password = self.passwordst[:-difference]
+		print(self.password)
+		
 
-	def checkContains(self):
-		output = bool(re.match("(?=.*\d.*)(?=.*[a-z])(?=.*[A-Z])", self.password))
-		print(output)
+	def checkContains(self, inputString):
+		output = bool(re.match("(?=.*\d.*)(?=.*[a-z])(?=.*[A-Z])", inputString))
 		return output
+			# print("Contains more than just Numbers, Lower-Case Letters and Upper-Case Letters")
 
 	def occurenceCounter(self):
-		# This method is inefficient and doesnt work with succession issues
-		res = Counter(self.password)
-		print(res)
-		return False
+		groups = groupby(self.password)
+		result = [(label, sum(1 for _ in group)) for label, group in groups]
+		print(result)
+		newPassword = ""
+		for key, value in result:
+			if value >= 3:
+				self.result += value - 2
+			if value == 1:
+				newPassword = newPassword + key
+			else:
+				newPassword = newPassword + key + key
+		self.password = newPassword
+
+	def occurenceCheckCounter(self):
+		groups = groupby(self.password)
+		result = [(label, sum(1 for _ in group)) for label, group in groups]
+		print(result)
+		newPassword = ""
+		substituteString = "1Z8lx5P"
+		for key, value in result:
+			alpahnumeric = key.isalnum()
+			if value >= 3:
+				self.result += value - 2
+				value = 2
+			if (alpahnumeric == False):
+				key = substituteString[1]
+				substituteString = substituteString[:-1]
+				self.result += value
+			if value == 1:
+				newPassword = newPassword + key
+			else:
+				newPassword = newPassword + key + key
+		self.password = newPassword
+		digitChange = False
+		if(self.checkContains(self.password) == False and self.result < 2):
+			if((not self.password.islower() and not self.password.isupper())==False and self.result == 0):
+				self.result +=1
+				digitChange = True
+			if((any(i.isdigit() for i in self.password)) == False):
+				if (self.result <= 1 and digitChange == True):
+					self.result +=1
+				elif(self.result == 0):
+					self.result +=1
+
+			print(self.password)
+		
+
+	def loadCommonPasswords(self):
+		if self.fullList == []:
+			with open(self.filePath, 'r') as infile:
+				data = infile.read()
+				self.fullList = data.splitlines()
+		completeBool = False
+		while(completeBool == False):
+			result = [ele for ele in self.fullList if(ele in self.password)]
+			if(result != []):
+				print(result)
+				smallestElment = min(result, key=len)
+				self.result += 1
+				if(smallestElment[-1] == "z"):
+					newSmallest = smallestElment[:-1] + "1"
+				else:
+					newSmallest = smallestElment[:-1] + "z"
+				self.password = self.password.replace(smallestElment, newSmallest)
+			else:
+				completeBool = True
+
+
+
+
 
 	
--- task_one/UnitTest.py
+++ task_one/UnitTest.py
@@ -1,23 +1,52 @@
 import unittest
 from CommonPasswords import CommonPasswords
 class UnitTest(unittest.TestCase):
-	print("hello")
-	def test1():
+	def testOneLetter():
 		password = "z"
 		expectedOutput = 6
 		common = CommonPasswords(password)
-		print(common.result)
-	def test2():
+		assert common.result == expectedOutput
+	def testTooShort():
 		password = "aA1"
 		expectedOutput = 4
 		common = CommonPasswords(password)
-		print(common.result)
-	def test3():
+		assert common.result == expectedOutput
+	def TestPass():
 		password = "1377C0d3"
 		expectedOutput = 0
 		common = CommonPasswords(password)
-		print(common.result)
+		assert common.result == expectedOutput
+	def testThreeConsecutiveLetters():
+		password = "13777C0d3"
+		expectedOutput = 1
+		common = CommonPasswords(password)
+		assert common.result == expectedOutput
+	def testInCommonPasswordsExactly():
+		password = "BvtTest123"
+		expectedOutput = 1
+		common = CommonPasswords(password)
+		assert common.result == expectedOutput
+	def testInCommonPasswords():
+		password = "512345678yY"
+		expectedOutput = 1
+		common = CommonPasswords(password)
+		assert common.result == expectedOutput
+	def testContainsSymbols():
+		password = "1234567--"
+		expectedOutput = 3
+		common = CommonPasswords(password)
+		assert common.result == expectedOutput
+	def testDoesNotContainUpperCase():
+		password = "a67hhnp7t"
+		expectedOutput = 1
+		common = CommonPasswords(password)
+		assert common.result == expectedOutput
 	if __name__ == "__main__":
-	    test1()
-	    test2()
-	    test3()
+	    testOneLetter()
+	    testTooShort()
+	    TestPass()
+	    testThreeConsecutiveLetters()
+	    testInCommonPasswordsExactly()
+	    testInCommonPasswords()
+	    testContainsSymbols()
+	    testDoesNotContainUpperCase()
